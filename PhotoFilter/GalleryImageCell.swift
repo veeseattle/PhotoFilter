4cc58ac0ac50d145759e7c8cedb41d102517278e
Vania Kurniawati <koala@MacBook-Air.local>
2015-01-16 21:48:12 -0800

    first commit

---
+++ PhotoFilter/GalleryImageCell.swift
@@ -0,0 +1,24 @@
+//
+//  GalleryImageCell.swift
+//  PhotoFilter
+//
+//  Created by Vania Kurniawati on 1/14/15.
+//  Copyright (c) 2015 Vania Kurniawati. All rights reserved.
+//
+
+import UIKit
+
+class GalleryImageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PhotoFilter/Thumbnail.swift
@@ -0,0 +1,9 @@
+//
+//  Thumbnail.swift
+//  PhotoFilter
+//
+//  Created by Vania Kurniawati on 1/13/15.
+//  Copyright (c) 2015 Vania Kurniawati. All rights reserved.
+//
+
+import Foundation
